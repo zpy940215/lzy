a37f917c162dcb49012d52c376e31f8d00c8a047
Administrator <Administrator@GG8QNNEDY9KP6X2>
2019-09-26 09:23:35 +0800

    6

--- lzy-web/src/main/java/com/pro/msv/action/LoginAction.java
+++ lzy-web/src/main/java/com/pro/msv/action/LoginAction.java
@@ -1,4 +1,4 @@
-package com.pro.msv.controller;
+package com.pro.msv.action;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- lzy-web/src/main/java/com/pro/msv/action/ModuleAction.java
+++ lzy-web/src/main/java/com/pro/msv/action/ModuleAction.java
@@ -1,4 +1,4 @@
-package com.pro.msv.controller;
+package com.pro.msv.action;
 
 import java.util.ArrayList;
 import java.util.List;
